02b10598ac6565899c55ba7c205940bd747a01b9
sunrab1822 <wbarnus99@gmail.com>
2023-11-23 12:45:52 +0100

    alma

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -2,10 +2,10 @@
     <div>
         <h2>Sign Up</h2>
         <label for="name">
-            <input type="text" id="name" placeholder="name">
+            <input type="text" id="name" v-model="username" placeholder="name">
         </label>
         <label for="text">
-            <input type="text" id="email" placeholder="email">
+            <input type="text" id="email" placeholder="email" v-model="email">
         </label>
         <label for="password">
             <input v-model="password" @keyup="valtozas" type="password" placeholder="password" id="password">
@@ -14,7 +14,7 @@
         <label for="password-repeat">
             <input type="text" v-model="password_repeat" placeholder="password-repeat" id="password-repeat">
         </label>
-        <button :disabled="isDesabled">signup</button>
+        <button :disabled="isDesabled" @click="signup">signup</button>
     </div>
 </template>
 
@@ -23,6 +23,9 @@ import { ref } from 'vue';
 
     const isDesabled = ref(true)
     let password = ref("")
+    let password_repeat = ref("")
+    let username = ref("")
+    let email = ref("")
 
     const valtozas = () => {
         if(password.value == ""){
@@ -32,5 +35,20 @@ import { ref } from 'vue';
             isDesabled.value = false
         }
     }
-    
+
+    const signup = () => {
+        if(email.value == "" || username.value == ""){
+            alert("minden adatot ki kell tölteni")
+            return
+        }
+
+        if (password.value != password_repeat.value)
+        {
+            alert("nem ugyanaz a jelszot adta meg")
+            return
+        }
+
+    }
+
+
 </script>
